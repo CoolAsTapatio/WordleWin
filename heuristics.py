5830b8ceca1015f518dbcf82765e37c4fe1b5fa3
Kids <Kids@Steves-MacBook-Pro.local>
2023-05-08 10:04:21 -1000

    working on a letter frequency version and a word frequency version

--- heuristics.py
+++ heuristics.py
@@ -1,6 +1,12 @@
-def sorter(frequency, word):#formula = frequency, reciprocal letter frequency (gets less important each guess), unique letters
+def sorter(word):#formula = frequency, reciprocal letter frequency (gets less important each guess), unique letters
 	letter_value = {'a':1 , 'b':3, 'c':3, 'd':2, 'e':1, 'f':4, 'g':2, 'h':4, 'i':1, 'j':8, 'k':5, 'l':1, 'm':3, 'n':1, 'o':1, 'p':3, 'q':10, 'r':1, 's':1, 't':1, 'u':1, 'v':8, 'w':4, 'x':8, 'y':4, 'z':10}
+	letters_in_word = []
 	letter_score = 0
-	for i in word:
-		letter_score += letter_value[i]
-	return (100*frequency/letter_score)/2
+	for j in word:
+		letter_score += letter_value[j]
+		if letter_value[j] in letters_in_word:
+			letter_score/2
+		letters_in_word.append(letter_value[j])
+		
+#	print(letter_score)
+	return letter_score
